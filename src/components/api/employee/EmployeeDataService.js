841028a1cd1934221aa8a1d70de1e2e867ca2ba8
kuanyilee85 <kuanyilee85@gmail.com>
2021-02-28 13:05:29 -0500

    add URL Constants

--- src/components/api/employee/EmployeeDataService.js
+++ src/components/api/employee/EmployeeDataService.js
@@ -1,26 +1,29 @@
 import Axios from 'axios';
-import { API_URL, JPA_API_URL } from '../../../Constants';
+import { JPA_API_URL } from '../../../Constants';
 
 class EmployeeDataService {
   // GET all
-  retriveAllEmployee(name) {
-    return Axios.get(`${JPA_API_URL}/users/${name}/employees/`);
+  retriveAllEmployee(username) {
+    return Axios.get(`${JPA_API_URL}/users/${username}/employees/`);
   }
   // GET single
-  retriveEmployee(name, id) {
-    return Axios.get(`${JPA_API_URL}/users/${name}/employees/${id}`);
+  retriveEmployee(username, id) {
+    return Axios.get(`${JPA_API_URL}/users/${username}/employees/${id}`);
   }
   // DELETE
-  deleteEmployee(name, id) {
-    return Axios.delete(`${API_URL}/users/${name}/employees/${id}`);
+  deleteEmployee(username, id) {
+    return Axios.delete(`${JPA_API_URL}/users/${username}/employees/${id}`);
   }
   // PUT
-  updateEmployee(name, id, employee) {
-    return Axios.put(`${API_URL}/users/${name}/employees/${id}`, employee);
+  updateEmployee(username, id, employee) {
+    return Axios.put(
+      `${JPA_API_URL}/users/${username}/employees/${id}`,
+      employee
+    );
   }
   // CREATE
-  createEmployee(name, employee) {
-    return Axios.post(`${API_URL}/users/${name}/employees/`, employee);
+  createEmployee(username, employee) {
+    return Axios.post(`${JPA_API_URL}/users/${username}/employees/`, employee);
   }
 }
 
